685c1d1cfbc0d56bb5ba0dcf8ad7291f89da71df
zilbers <67857576+zilbers@users.noreply.github.com>
2020-08-06 16:29:20 +0300

    Working app
    Added frontend and tweeked (used the received server, but added entry points and added some logic) the server.
    to see full commit list:
    https://github.com/zilbers/shopping-list/pull/1

--- index.js
+++ index.js
@@ -1,101 +1,95 @@
 const express = require("express");
 const app = express();
 app.use(express.json());
+app.use(express.static(`${__dirname}/src`));
+app.use(logger);
+
+let requestID = 0;
+
+function logger(req, res, next) {
+  requestID++;
+  console.log(`Request fired: ${req.url} 
+Method: ${req.method}
+Request #${requestID}
+`);
+  next();
+}
 
 const MyList = [
-        {id: "1", productName: "milk"},
-        {id: "2", productName: "bread"},
-        {id: "3", productName:"cheese"},
-        {id: "4", productName: "water"},
-        {id: "5", productName: "oil"}
-    ];
+  { id: "1", product: "milk" },
+  { id: "2", product: "bread" },
+  { id: "3", product: "cheese" },
+  { id: "4", product: "water" },
+  { id: "5", product: "oil" },
+];
 
+let shoppingList = [];
+
+// Home entry point, loads HTML
+app.get("/", (req, res) => {
+  console.log("Welcome to my shopping list app!");
+  res.sendFile("src/index.html");
+});
+
+// Sends the product list
+app.get("/shop", (req, res) => {
+  res.send(MyList);
+});
+
+// Sends customer's added items list
 app.get("/products", (req, res) => {
-    res.send(MyList);
-  });
+  res.send(shoppingList);
+});
 
 app.get("/products/:productId", (req, res) => {
-      MyList.forEach( (item) => {
-           if (item.id === req.params.productId) {
-              res.send(item.productName);
-          }
-      });
+  MyList.forEach((item) => {
+    if (item.id === req.params.productId) {
+      res.send(item);
+    }
   });
-
-  app.post("/products", (req, res) => {
-      MyList.push(req.body);             
-    res.send(req.body);
-  });
-
-  app.put("/products/:productId", (req, res) => {
-    MyList.forEach((item) => {
-        if (item.id === req.params.productId) {
-           MyList.splice(MyList.indexOf(item),1,req.body)
-           res.send(MyList);
-       }
-   });
 });
 
+// Saves customers cart (added items) in the server
+app.post("/products", (req, res) => {
+  shoppingList = [];
+  if (req.body.length > 1) {
+    req.body.forEach((productToInsert) => {
+      shoppingList.push(productToInsert);
+    });
+  } else {
+    shoppingList.push(req.body);
+  }
+  res.send(shoppingList);
+});
 
+// Adding new item to the product list
+app.post("/new", (req, res) => {
+  if (req.body.length > 1) {
+    req.body.forEach((productToAdd) => {
+      MyList.push(productToAdd);
+    });
+  } else {
+    MyList.push(req.body);
+  }
+  res.send(MyList);
+});
+
+app.put("/products/:productId", (req, res) => {
+  MyList.forEach((item) => {
+    if (item.id === req.params.productId) {
+      MyList.splice(MyList.indexOf(item), 1, req.body);
+      res.send(MyList);
+    }
+  });
+});
 
 app.delete("/products/:productId", (req, res) => {
-    MyList.forEach((item) => {
-        if (item.id === req.params.productId) {
-           MyList.splice(MyList.indexOf(item),1);
-           res.send(MyList);
-       } 
-   });
+  MyList.forEach((item) => {
+    if (item.id === req.params.productId) {
+      MyList.splice(MyList.indexOf(item), 1);
+      res.send(MyList);
+    }
+  });
 });
 
-
-
-app.listen(3000);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.listen(3000, () => console.log("Listening to customer on port #3000!"));
---
+++ src/main.js
@@ -0,0 +1,183 @@
+console.log("main.js is on!");
+
+let shoppingList = [];
+let id = 5;
+
+/** Adds product to the cart
+ * @param {*} event the event who set the lsitener
+ */
+const addProduct = (event) => {
+  let product = event.target;
+  if (product.classList[0] !== "product") return;
+  let amount = parseInt(product.classList[1]);
+  productToAdd = {
+    id: product.id,
+    product: product.innerHTML,
+    amount: amount,
+  };
+  if (shoppingList.length === 0) {
+    shoppingList.push(productToAdd);
+  } else {
+    let check = 0;
+    shoppingList.forEach((item, i) => {
+      if (item.id === product.id) {
+        shoppingList[i].amount++;
+        check++;
+      }
+    });
+    if (!check) {
+      shoppingList.push(productToAdd);
+    }
+  }
+  updateCart();
+  console.log(shoppingList);
+};
+
+/** Cart is hidden by default, by using this function it shows it
+ */
+const showCart = () => {
+  let cart = document.querySelector("#cart");
+  cart.hidden = cart.hidden ? false : true;
+  if (cart.hidden) {
+    cart.style.visibility = "visible";
+  } else {
+    cart.style.visibility = "hidden";
+  }
+};
+
+/** Gets the product list from server
+ */
+async function getProductsList() {
+  try {
+    const products = await axios.get(`http://localhost:3000/shop`);
+    console.log(products.data);
+    printProductsList(products.data);
+  } catch (err) {
+    console.error(`Couldn't get product list! error: ${err}`);
+  }
+}
+
+/** Saves the items in the cart to the server
+ */
+async function saveCart() {
+  try {
+    const products = await axios.post(
+      `http://localhost:3000/products`,
+      shoppingList
+    );
+    console.log(products.data);
+  } catch (err) {
+    console.error(`Couldn't save product list! error: ${err}`);
+  }
+}
+
+/** Gets the cart from server with the items saved
+ */
+async function getItemsFromServer() {
+  try {
+    const products = await axios.get(`http://localhost:3000/products`);
+    shoppingList = products.data;
+    updateCart();
+  } catch (err) {
+    console.error(`Couldn't get product list! error: ${err}`);
+  }
+}
+
+/** Adds item to product list of the shop on server 
+ */
+async function addProductToShop() {
+  let newItem = document.querySelector("#newItem");
+  try {
+    if (newItem.value === "") throw "Cant be empty!";
+    id++;
+    const products = await axios.post(`http://localhost:3000/new`, {
+      id: `${id}`,
+      product: newItem.value,
+    });
+    console.log({ id: id, product: newItem.value });
+    console.log(products.data);
+    getProductsList();
+    newItem.value = "";
+  } catch (err) {
+    console.error(`Couldn't add product! error: ${err}`);
+  }
+}
+
+/** Creates a table and prints the items
+ */
+function updateCart() {
+  let cart = document.querySelector("#cart");
+  cart.innerHTML = "";
+  createElementWithData("", "cartTable", "table", "#cart");
+  createElementWithData("", "headTr", "tr", "#cartTable");
+  createElementWithData("ID", "idSlot", "th", "#headTr");
+  createElementWithData("Product", "productSlot", "th", "#headTr");
+  createElementWithData("Amount", "amountSlot", "th", "#headTr");
+  shoppingList.forEach((item) => {
+    createElementWithData("", `${item.product}row`, "tr", "#cartTable");
+    createElementWithData(item.id, "", "td", `#${item.product}row`);
+    createElementWithData(item.product, "", "td", `#${item.product}row`);
+    createElementWithData(item.amount, "", "td", `#${item.product}row`);
+  });
+}
+
+/** Creates the hidden cart on loading
+ */
+function printCart() {
+  let cart = createElementWithData("Cart is empty!", "cart");
+}
+
+/** Prints the shopping list with products from server
+ * @param {Json} products
+ */
+function printProductsList(products) {
+  let shop = document.querySelector("#productsShop");
+  shop.innerHTML = "";
+  products.forEach((item) => {
+    let productDiv = createElementWithData(
+      item.product,
+      item.id,
+      "div",
+      "#productsShop"
+    );
+    productDiv.className = "product 1";
+  });
+}
+
+/** Uses query to find an element and adds an event listener to it
+ * @param {string} element the item to find in the HTML page
+ * @param {function} callback function to use as callback in the event listener
+ * @param {string} type of event to listen to
+ */
+function myQueryAndEventListener(element, callback, type = "click") {
+  let itemAddEvent = document.querySelector(element);
+  itemAddEvent.addEventListener(type, callback);
+}
+
+/** Creates element in the html and adds to it innerHTML and id and can choose what type of object and where to place
+ * @param {string} data The data to place inside the object
+ * @param {string} id The id of the object
+ * @param {string} type Type of object' div\span for example
+ * @param {string} father Where to append the element
+ */
+function createElementWithData(
+  data = "",
+  id = "",
+  type = "div",
+  father = "body"
+) {
+  let fatherElement = document.querySelector(father);
+  let element = document.createElement(type);
+  element.innerHTML = data;
+  element.id = id;
+  fatherElement.appendChild(element);
+  return element;
+}
+
+printCart();
+getItemsFromServer();
+myQueryAndEventListener("#saveCart", saveCart);
+myQueryAndEventListener("#shopButton", getProductsList);
+myQueryAndEventListener("#cartButton", showCart);
+myQueryAndEventListener("#addButton", addProductToShop);
+document.addEventListener("click", addProduct);
